02d847c4fb842762e2e028fc1ee2c71d389c8467
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-05-25 19:11:45 +0200

    Initial Commit

---
+++ LiquidGlassMorphingTabBar/LiquidGlassMorphingTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassMorphingTabBarApp.swift
+//  LiquidGlassMorphingTabBar
+//
+//  Created by Alex De Pasquale on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassMorphingTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
